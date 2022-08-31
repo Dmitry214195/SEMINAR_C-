b03155097eef3c13ac5ea89f3133bfa659574a2a
Dmitry Artamonov <dmitri.artanonow@yadex.ru>
2022-08-31 15:51:31 +0500

    возведение в квадрат

---
+++ Lesson1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Определение входного числа
+Console.WriteLine("Ведите число: ");
+int number = int.Parse(Console.ReadLine());
+
+// Получение квадрата входного числа
+int result = number * number;
+Console.WriteLine($"Квадрат числа {number} = {result}");
+
+// Получение квадрата числа математической функцией
+int result_2 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} = {result}");
